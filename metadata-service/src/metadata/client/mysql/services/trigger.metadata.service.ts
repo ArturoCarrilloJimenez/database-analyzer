bfd73b86da8d92685d7351993152b1fc8c936471
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2025-10-31 14:57:56 +0100

    update: triggers info of SHOW CREATE TRIGGER

--- metadata-service/src/metadata/client/mysql/services/trigger.metadata.service.ts
+++ metadata-service/src/metadata/client/mysql/services/trigger.metadata.service.ts
@@ -15,13 +15,41 @@ export class MySQLTriggerMetadataService extends AbstractTriggerService {
     database: string,
   ): Promise<BasicTriggerQuery[]> {
     try {
-      const trigger = connectionDb('information_schema.TRIGGERS')
+      const trigger = await connectionDb('information_schema.TRIGGERS')
         .select<
           BasicTriggerQuery[]
         >(['TRIGGER_NAME as name', 'EVENT_MANIPULATION as tableName', 'EVENT_OBJECT_TABLE as operation', 'ACTION_TIMING as ejecute'])
         .where('TRIGGER_SCHEMA', database);
 
-      return trigger;
+      // Obtengo el código del trigger
+      const detailedTriggersPromises = trigger.map(async (t) => {
+        try {
+          // Ejecución del comando DDL especial para obtener el código
+          const [result]: any = await connectionDb.raw(
+            'SHOW CREATE TRIGGER ??',
+            [t.name],
+          );
+
+          // MySQL devuelve el código en la columna 'SQL Statement' o 'Create Trigger'
+          const codeStatement =
+            result[0]['SQL Statement'] || result[0]['Create Trigger'];
+
+          return {
+            ...t,
+            code: codeStatement, // Incluimos el código completo
+          };
+        } catch (error) {
+          this.logger.error(
+            `Error fetching code for trigger: ${t.name}`,
+            error,
+          );
+          // Devolvemos el trigger básico sin código si falla la obtención del DDL
+          return t;
+        }
+      });
+
+      // Esperar a que todas las promesas de SHOW CREATE se resuelvan
+      return Promise.all(detailedTriggersPromises);
     } catch (error) {
       this.logger.error('Error fetching tables', error);
       throw createRpcError({
